0a5a2d34703e87db31f9f03675b70e6f79a33db6
wallis6n <54006485+wallis6n@users.noreply.github.com>
2020-03-12 18:48:17 -0300

    Feature de manter o darkmode ao atualizar a página.

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-function nigthMode() {
+
+document.cookie.replace(/(?:(?:^|.*;\s*)darkmode\s*\=\s*([^;]*).*$)|^.*$/, "$1") == "true" ? nigthMode(true) : nigthMode(false)
+
+function nigthMode(CookieMode) {
     let check = document.querySelector('input');
     document.querySelector('input').addEventListener('click', () => {
         // A propriedade `checked` retorna um `boolean`.
         console.log(check.checked);
     });
+
     
-    if(check.checked == false){
-        document.getElementsByTagName("body")[0].style.backgroundColor = "#FFFFFF";
-        document.getElementsByTagName("body")[0].style.color = "#111111";
-    }
-    else {
+    if(CookieMode == true){
+        check.checked = true;
         document.getElementsByTagName("body")[0].style.backgroundColor = "#192734";
         document.getElementsByTagName("body")[0].style.color = "#F8F8FF";
+    }
+    else if (check.checked == false && CookieMode != true){
+        document.cookie = "darkmode=false;"
+        document.getElementsByTagName("body")[0].style.backgroundColor = "#FFFFFF";
+        document.getElementsByTagName("body")[0].style.color = "#111111";
+    } else{
+        document.cookie = "darkmode=true;"
+        document.getElementsByTagName("body")[0].style.backgroundColor = "#192734";
+        document.getElementsByTagName("body")[0].style.color = "#F8F8FF";
+
     }
     
 }
