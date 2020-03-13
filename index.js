053ceadffd82c22d6ba3c79579460f960868b29c
wallis6n <weedeadpixels@gmail.com>
2020-03-13 14:52:35 -0300

    Adicionando a comemoração ao pra quando o bruxo ser solto

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 
 document.cookie.replace(/(?:(?:^|.*;\s*)darkmode\s*\=\s*([^;]*).*$)|^.*$/, "$1") == "true" ? nigthMode(true) : nigthMode(false)
+window.onload =  bruxoTaSolto();
+
+function bruxoTaSolto(){
+    let status = document.getElementById('situation').textContent;
+    let audio = document.getElementById("comemoracao");
+    if(status == "Sim"){
+        confetti.start();
+        audio.play();
+    }
+}
 
 function nigthMode(CookieMode) {
     let check = document.querySelector('input');
@@ -7,7 +17,6 @@ function nigthMode(CookieMode) {
         // A propriedade `checked` retorna um `boolean`.
         console.log(check.checked);
     });
-
     
     if(CookieMode == true){
         check.checked = true;
@@ -15,11 +24,11 @@ function nigthMode(CookieMode) {
         document.getElementsByTagName("body")[0].style.color = "#F8F8FF";
     }
     else if (check.checked == false && CookieMode != true){
-        document.cookie = "darkmode=false;"
+        document.cookie = "darkmode=false;";
         document.getElementsByTagName("body")[0].style.backgroundColor = "#FFFFFF";
         document.getElementsByTagName("body")[0].style.color = "#111111";
     } else{
-        document.cookie = "darkmode=true;"
+        document.cookie = "darkmode=true;";
         document.getElementsByTagName("body")[0].style.backgroundColor = "#192734";
         document.getElementsByTagName("body")[0].style.color = "#F8F8FF";
 
